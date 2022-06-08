5a54c7e1b973dd6cdd647bd4cb017a6b117897b0
James R <jimmer1609@gmail.com>
2022-06-08 19:53:58 +0900

    configured lab to display tasks to web page

---
+++ controllers/tasks_controller.py
@@ -0,0 +1,16 @@
+from asyncio import all_tasks
+from flask import Flask, render_template
+from flask import Blueprint
+
+from repositories import task_repository
+
+tasks_blueprint = Blueprint("tasks", __name__)
+
+@tasks_blueprint.route("/tasks")
+def tasks():
+    tasks = task_repository.select_all()
+    return render_template("tasks/index.html", all_tasks=tasks)
+
+@tasks_blueprint.route("/tasks/create", methods=['POST'])
+def make_task():
+    pass
